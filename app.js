17b3df261535e71d29954551c9914386bc40d10c
Sydney Kleingartner <43350106+sydneykleingartner@users.noreply.github.com>
2019-05-29 14:34:37 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+//default mode: front-facing camera
+var constraints = { video: { facingMode: "user"}, audio: false};
+
+//ask for access to the camera (not audio)
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+	navigator.mediaDevices.getUserMedia({video:true}).then(function(stream) {
+		video.srcObject = stream;
+		video.play();
+	})
+}
+
+//define constants of all the parts that were created
+const cameraView = document.querySelector("#camera--view"),
+	  cameraOutput = document.querySelector("#camera--output"),
+	  cameraSensor = document.querySelector("#camera--sensor"),
+	  cameraTrigger = document.querySelector("#camera--trigger")
+
+
+//access the camera & stream to the cameraView
+function cameraStart() {
+	navigator.mediaDevices
+		.getUserMedia(constraints)
+		.then(function(stream) {
+		track = stream.getTracks()[0];
+		cameraView.srcObject = stream;
+	})
+	.catch(function(error) {
+		console.error("Oops. Something is broken.", error)
+	});
+}
+
+//take a picture when the button is tapped
+
+cameraTrigger.onclick = function() {
+	cameraSensor.width = cameraView.videoWidth;
+	cameraSensor.height = cameraView.videoHeight;
+	cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+	cameraOutput.src = cameraSensor.toDataURL("image/webp");
+	cameraOutput.classList.add("taken");
+};
+
+
+//start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
+
+if ('serviceWorker' in navigator) {
+	console.log('CLIENT: service worker registration in progress.');
+	navigator.serviceWorker.register( '/camera-app/part-2/sw.js', { scope : ' '}).then(function() {
+		console.log('CLIENT: service worker registration complete.');
+	}, function() {
+		console.log('CLIENT: service worker registration failure.');
+	});
+}
+else {
+	console.log('CLIENT: service worker is not supported.');
+}
